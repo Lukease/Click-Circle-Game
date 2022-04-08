5fdb35582a6866c30dd6953f731e131b4934df72
Lukease <lukpawlowski@gmail.com>
2022-04-08 12:50:09 +0200

    add enter before return, if, changes return in createTimerBox and createCircleBox, add create timer to createTimerBox, remove Stop Time, add if to addColor

--- click/main.js
+++ click/main.js
@@ -4,38 +4,40 @@ const createTimerBox = () => {
     const firstBox = document.createElement('div')
     firstBox.classList.add('app__box')
     app.appendChild(firstBox)
-    return document.querySelector('.app__box')
+
+    const timer = document.createElement('div')
+    timer.classList.add('timer')
+
+    firstBox.appendChild(timer)
+
+    return timer
 }
 
 const createCircleBox = () => {
     const secondBox = document.createElement('div')
     secondBox.classList.add('app__box')
     app.appendChild(secondBox)
-    return document.querySelectorAll('.app__box')[1]
+
+    return secondBox
 }
-
-const timer = document.createElement('div')
-timer.classList.add('timer')
-createTimerBox().appendChild(timer)
-
-const stopTime = document.createElement('p')
-stopTime.innerHTML = 'Stop Time'
 
 let seconds = 60
 let minutes = parseInt(seconds / 60, 10)
 seconds = seconds % 60
+const timer = createTimerBox()
 
 const interval = setInterval(() => {
     if (seconds === 0 && minutes === 1) {
         minutes -= 1
         seconds = 60
     }
+
+    seconds -= 1
+
     if (seconds === 0) {
         clearInterval(interval)
-        createTimerBox().appendChild(stopTime)
         alert(`${points} : Total Score`)
     }
-    seconds -= 1
     timer.innerHTML = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
 }, 1000)
 
@@ -61,15 +63,17 @@ scoreBox.value = points
 const allCircles = document.querySelectorAll('.circle')
 
 const addColor = setInterval(() => {
-    allCircles.forEach(object => {
-        object.classList.remove('circle__red')
-        object.classList.remove('circle__green')
-    })
-    let randomNumber = Math.floor(Math.random() * 5)
-    let coloredCircle = allCircles[randomNumber]
-    coloredCircle.classList.add('circle__red')
-    document.querySelector('.app__container').addEventListener('click', scorePoint)
 
+    if (seconds > 0) {
+        allCircles.forEach(object => {
+            object.classList.remove('circle__red')
+            object.classList.remove('circle__green')
+        })
+        let randomNumber = Math.floor(Math.random() * 5)
+        let coloredCircle = allCircles[randomNumber]
+        coloredCircle.classList.add('circle__red')
+        document.querySelector('.app__container').addEventListener('click', scorePoint)
+    }
 }, 1000)
 
 const scorePoint = event => {
